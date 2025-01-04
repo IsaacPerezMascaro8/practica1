1a5b10aacb4427626d334eab5f82875fba21be54
Isaac Perez Mascaro <ipermas@upv.edu.es>
2025-01-04 11:08:31 +0100

    Correcion List.h

--- List.h
+++ List.h
@@ -9,9 +9,9 @@ class List {
         virtual void prepend(T e) = 0;
         virtual T remove(int pos) = 0;
         virtual T get(int pos) = 0;
-        int search(T e) = 0;
-        bool empty() = 0;
-        int size() = 0;
+        virtual int search(T e) = 0;
+        virtual bool empty() = 0;
+        virtual  int size() = 0;
 };
 
 #endif
