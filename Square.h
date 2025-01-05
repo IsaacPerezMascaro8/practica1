75cba5ea62e37fd3e4a095296b7820d2eee372e4
Isaac Perez Mascaro <ipermas@upv.edu.es>
2025-01-05 12:25:24 +0100

    Añadido Square.h y Square.cpp

---
+++ Square.h
@@ -0,0 +1,27 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <iostream>
+#include <stdexcept>
+#include "Rectangle.h"
+
+using namespace std;
+
+class Square : public Rectangle {
+public:
+    // Constructores
+    Square();  // Constructor por defecto
+    Square(string color, Point2D* vertices);  // Constructor con parámetros
+
+    // Sobrescritura de métodos
+    void set_vertices(Point2D* vertices) override;  // Modificar los vértices (sobrescrito)
+    virtual void print() const override;  // Imprime información del cuadrado
+
+    friend ostream& operator<<(ostream &out, const Square &square);
+
+private:
+    static bool check(Point2D* vertices);  // Verifica si los vértices conforman un cuadrado válido
+};
+
+#endif
+
